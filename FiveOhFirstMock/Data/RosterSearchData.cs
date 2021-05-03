4e0bb4f54f467e71d2a491660f08c92db29c8bbe
Andrew Bounds <andybounds@gmail.com>
2021-05-03 12:22:05 -0400

    Redesigned trooper info and added filter to the roster

---
+++ FiveOhFirstMock/Data/RosterSearchData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstMock.Data
+{
+    public class RosterSearchData
+    {
+        public string IdFilter { get; set; } = "";
+        public string UsernameFilter { get; set; } = "";
+        public string UnitFilter { get; set; } = "";
+        public string RoleFilter { get; set; } = "";
+        public bool Ascending { get; set; } = true;
+    }
+}
